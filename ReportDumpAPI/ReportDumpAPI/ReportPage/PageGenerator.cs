d4e3a3070d17eeeaddef8d9ddc627b12e646252d
Sam <ArcticEcho@users.noreply.github.com>
2016-05-23 14:01:11 +0100

    Create dupe report page

--- ReportDumpAPI/ReportDumpAPI/ReportPage/PageGenerator.cs
+++ ReportDumpAPI/ReportDumpAPI/ReportPage/PageGenerator.cs
@@ -25,13 +25,14 @@ namespace ReportDumpAPI.ReportPage
         public static string GenerateReportPage(string jsonStr)
         {
             var json = JSON.Deserialize<Dictionary<string, object>>(jsonStr);
+            var isDupeReport = false;
             var html = template;
 
-            html = PatchReportType(html, json);
+            html = PatchReportType(html, json, out isDupeReport);
             html = PatchReportRoomID(html, json);
             html = PatchReportID(html, json);
             html = PatchReportSearchTag(html, json);
-            html = PatchReportContent(html, json);
+            html = PatchReportContent(html, json, isDupeReport);
 
             return html;
         }
@@ -58,10 +59,11 @@ namespace ReportDumpAPI.ReportPage
             return html.Replace("$SERVER_VERSION$", a);
         }
 
-        private static string PatchReportType(string html, Dictionary<string, object> json)
+        private static string PatchReportType(string html, Dictionary<string, object> json, out bool dupes)
         {
             var isDupesStr = json["is_filtered_duplicates"].ToString();
             var isDupes = bool.Parse(isDupesStr);
+            dupes = isDupes;
             return html.Replace("$REPORT_TYPE$", isDupes ? "Duplicate" : "Cherrypick");
         }
 
@@ -111,7 +113,7 @@ namespace ReportDumpAPI.ReportPage
             return html.Replace("$REPORT_TAG$", tags);
         }
 
-        private static string PatchReportContent(string html, Dictionary<string, object> json)
+        private static string PatchReportContent(string html, Dictionary<string, object> json, bool dupes)
         {
             var qsJson = json["questions"].ToString();
             var qs = JSON.Deserialize<Dictionary<string, object>[]>(qsJson);
@@ -119,7 +121,7 @@ namespace ReportDumpAPI.ReportPage
 
             for (var i = 0; i < qs.Length; i++)
             {
-                list.AppendLine(GetQuestionHtml(qs[i]));
+                list.AppendLine(GetQuestionHtml(qs[i], dupes));
 
                 if (i >= 0 && i < qs.Length - 1)
                 {
@@ -132,7 +134,7 @@ namespace ReportDumpAPI.ReportPage
             return html.Replace("$REPORT_CONTENT$", list.ToString());
         }
 
-        private static string GetQuestionHtml(Dictionary<string, object> json)
+        private static string GetQuestionHtml(Dictionary<string, object> json, bool dupes)
         {
             var html = new StringBuilder();
             var qID = json["question_id"];
@@ -140,17 +142,18 @@ namespace ReportDumpAPI.ReportPage
             var title = json["title"].ToString();
             title = title.Remove(0, 1).Substring(0, title.Length - 2);
             var score = json["score"].ToString();
-            var cvCount = json["close_vote_count"].ToString();
+            score = score.StartsWith("-") || score == "0" ? score : $"+{score}";
+            var age = json["creation_date"].ToString();
+            var views = json["view_count"].ToString();
             var answerCount = json["answer_count"].ToString();
             var accecptedAnsID = json["accepted_answer_id"].ToString();
-            var views = json["view_count"].ToString();
-            var age = json["creation_date"].ToString();
+            var cvCount = json["close_vote_count"].ToString();
 
             // Start of report.
             html.AppendLine("<div class=\"report\">");
 
             // Start of report header.
-            html.AppendLine("<div class=\"reportHeader\">");
+            html.AppendLine("<div class=\"reportSubContainer\">");
 
             // Report title.
             html.AppendLine("<h3 class=\"reportTitle\">");
@@ -160,7 +163,7 @@ namespace ReportDumpAPI.ReportPage
             html.AppendLine("</h3>");
 
             // Report score.
-            html.AppendLine("<div class=\"reportScore\">");
+            html.AppendLine("<div class=\"questionScore\">");
             html.AppendLine(score);
             html.AppendLine("</div>");
 
@@ -168,17 +171,17 @@ namespace ReportDumpAPI.ReportPage
             html.AppendLine("</div>");
 
             // Start of report stats.
-            html.AppendLine("<div class=\"reportStatsContainer\">");
+            html.AppendLine("<div class=\"reportSubContainer\">");
 
             // Question age.
             html.AppendLine("<div class=\"postAge\">");
-            html.AppendLine("<span class=\"statName\">Posted:</span> ");
+            html.AppendLine("<span class=\"valueName\">Posted:</span> ");
             html.AppendLine($"<span class=\"postTime\">{age}<span>");
             html.AppendLine("</div>");
 
             // View count.
             html.AppendLine("<div class=\"viewCount\">");
-            html.AppendLine($"<span class=\"statName\">Views:</span> ");
+            html.AppendLine($"<span class=\"valueName\">Views:</span> ");
             html.AppendLine(views);
             html.AppendLine("</div>");
 
@@ -191,18 +194,63 @@ namespace ReportDumpAPI.ReportPage
             {
                 html.AppendLine("<div class=\"answerCount\" style=\"color:#00C741\" title=\"This question has an accepted answer\">");
             }
-            html.AppendLine("<span class=\"statName\">Answers:</span> ");
+            html.AppendLine("<span class=\"valueName\">Answers:</span> ");
             html.AppendLine(answerCount);
             html.AppendLine("</div>");
 
             // Close vote count.
             html.AppendLine("<div class=\"closeVotes\">");
-            html.AppendLine("<span class=\"statName\">Close votes:</span> ");
+            html.AppendLine("<span class=\"valueName\">Close votes:</span> ");
             html.AppendLine(cvCount);
             html.AppendLine("</div>");
 
             // End of report stats.
             html.AppendLine("</div>");
+
+            if (dupes)
+            {
+                var comments = JSON.Deserialize<Dictionary<string, object>[]>(json["comments"].ToString());
+
+                if (comments.Length > 0)
+                {
+                    var targetID = comments[0]["duplicated_target_id"];
+                    var dupeLink = $"//stackoverflow.com/q/{targetID}";
+                    var dupeTitle = comments[0]["duplicated_target_title"].ToString();
+                    dupeTitle = dupeTitle.Remove(0, 1).Substring(0, dupeTitle.Length - 2);
+                    var dupeScore = comments[0]["duplicated_target_score"].ToString();
+                    dupeScore = dupeScore.StartsWith("-") || dupeScore == "0" ? dupeScore : $"+{dupeScore}";
+
+                    // Start of dupe target details.
+                    html.AppendLine("<div class=\"reportSubContainer\">");
+
+                    // Dupe target link.
+                    html.AppendLine("<div class=\"dupeTargetLink\">");
+                    html.AppendLine("<span class=\"valueName\">Duplicate of:</span> ");
+                    html.AppendLine($"<a target=\"_blank\" href=\"{dupeLink}\">");
+                    html.AppendLine(dupeTitle);
+                    html.AppendLine("</a>");
+                    html.AppendLine("</div>");
+
+                    // Dupe target score.
+                    html.AppendLine("<div class=\"questionScore\">");
+                    html.AppendLine(dupeScore);
+                    html.AppendLine("</div>");
+
+                    // End of dupe details.
+                    html.AppendLine("</div>");
+
+                    if (comments.Length == 2)
+                    {
+                        var opComment = comments[1]["body"].ToString();
+
+                        // OP response.
+                        html.AppendLine("<div class=\"opResponseContainer\">");
+                        html.AppendLine("<span class=\"valueName\">Response from OP:</span> ");
+                        html.AppendLine($"<span class=\"opResponseComment\">{opComment}</span>");
+                        html.AppendLine("</div>");
+                    }
+                }
+            }
 
             // End of report.
             html.AppendLine("</div>");
